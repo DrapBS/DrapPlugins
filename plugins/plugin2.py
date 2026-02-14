2b936c85bbce60299b83848609b917634eb05f6a
Drap <mrunittest@gmail.com>
2026-02-14 15:24:21 +0100

    Update plugin2.py

--- plugins/plugin2.py
+++ plugins/plugin2.py
@@ -1,4 +1,9 @@
 # ba_meta require api 9
 import babase
 
-drpmod{"Name": "Drap Super Mod", "Description": "هذا أول مود في متجري الجديد!", "Picture": "None", "Video": "None", "Version": "1.0"}
+drpmod{"Name": "Drap Super Mod",
+       "Description": "هذا أول مود في متجري الجديد!",
+       "Picture": "None",
+       "Video": "None",
+       "Version": "1.0"
+      }
